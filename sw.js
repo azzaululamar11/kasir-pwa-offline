ef0770cc49b79b2cc398e3f5f1cfe80fd3b134e7
azzaululamar11 <azzaululamar11@gmail.com>
2026-03-23 00:46:33 +0700

    sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'kasir-offline-v1';
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['/', '/index.html', '/manifest.json'])));
+});
+self.addEventListener('fetch', (e) => {
+    if (e.request.url.includes('script.google.com')) return; // API jangan dicache
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
