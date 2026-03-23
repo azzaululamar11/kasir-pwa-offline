3ac603e0dcaf9fce0134435bcbf0d8fd7885a703
azzaululamar11 <azzaululamar11@gmail.com>
2026-03-23 16:48:22 +0700

    Updatev6 sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kasir-offline-v5';
+const CACHE_NAME = 'kasir-offline-v6';
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['/', '/index.html', '/manifest.json'])));
 });
